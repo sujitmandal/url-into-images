c73a694192b44eda27cd5acadcc0e8f6f6ecd4ac
Sujit Mandal <49348037+sujitmandal@users.noreply.github.com>
2020-09-12 23:46:59 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -22,6 +22,7 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.0',
+        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
